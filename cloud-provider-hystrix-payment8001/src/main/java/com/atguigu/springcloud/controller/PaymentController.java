3c39ee6b51adc3b848b0b2d36ed538cfbdf42349
dylan <lichien001@163.com>
2020-03-09 20:05:26 +0800

    完成hystrix

---
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,69 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.service.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Resource
+    private PaymentService paymentService;
+
+    @Value("${server.port}")
+    private String servicePort;
+
+    /**
+     * 正常访问
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/payment/hystrix/ok/{id}")
+    public String paymentInfo_OK(@PathVariable("id") Integer id) {
+        String result = paymentService.paymentInfo_OK(id);
+        log.info("*****result:" + result);
+        return result;
+    }
+
+    /**
+     * 超时访问
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/payment/hystrix/timeout/{id}")
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000")
+    })
+    public String paymentInfo_TimeOut(@PathVariable("id") Integer id) {
+        String result = paymentService.paymentInfo_timeout(id);
+        log.info("*****result:" + result);
+        return result;
+    }
+
+
+    public String paymentInfo_TimeOutHandler(Integer id) {
+        return  "线程池"+Thread.currentThread().getName()+"系统繁忙或者运行出错，请稍后再试 o(╥﹏╥)o";
+    }
+    /**
+     * 服务熔断
+     * http://localhost:8001/payment/circuit/32
+     * @param id
+     * @return
+     */
+    @GetMapping("/payment/circuit/{id}")
+    public String paymentCircuitBreaker(@PathVariable("id") Integer id) {
+        String result = paymentService.paymentCircuitBreaker(id);
+        log.info("***result:" + result);
+        return result;
+    }
+}
