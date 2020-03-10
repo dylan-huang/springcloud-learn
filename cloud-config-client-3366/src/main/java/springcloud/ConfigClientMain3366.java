e5725c5cf03aeb077da8c00023ef69a7fd209630
dylan <lichien001@163.com>
2020-03-10 21:12:04 +0800

    完成stream

---
+++ cloud-config-client-3366/src/main/java/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,18 @@
+package springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * 不重启刷新配置：
+ *   在配置更新完成后，client需要 发送post请求
+ *   eg： curl -X POST "http://localhost:3355/actuator/refresh"
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class,args);
+    }
+}
